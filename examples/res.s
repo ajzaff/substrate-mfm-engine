2607b995b74c684a100b1854fa3e3d8544571248
ajzaff <ajzaff@gmail.com>
2021-03-07 16:19:43 -0500

    Add res example

---
+++ examples/res.s
@@ -0,0 +1,28 @@
+.name "Res"
+.symbol "r"
+.fgcolor "#ff0"
+.bgcolor "#000"
+.author "Dave Ackley"
+.author "Alan Zaffetti"
+.license "GPL-2.0-or-later"
+.symmetries ALL
+.radius 1
+
+  push 1
+  getsite
+  getfield type
+  gettype Empty
+  equal
+  jumpzero end
+  push 0
+  push 0
+  getsite
+  setregister
+  push 0
+  push 1
+  getsite
+  setsite
+  push 1
+  push 0
+  getregister
+  setsite
