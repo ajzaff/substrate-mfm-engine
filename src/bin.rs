0c9e16c4ffd000ce83beb49599ecf4643a7cb149
ajzaff <ajzaff@gmail.com>
2021-03-12 23:45:50 -0500

    Cleanup ewac and remove deprecated failure deps

--- src/bin.rs
+++ src/bin.rs
@@ -1,9 +1,4 @@
 extern crate lalrpop_util;
-
-#[macro_use]
-extern crate failure;
-
-#[macro_use]
 extern crate lazy_static;
 
 mod ast;
@@ -12,104 +7,85 @@ mod code;
 
 use crate::code::Compiler;
 use atty::Stream;
-use quicli::prelude::*;
-use regex::Regex;
-use std::fmt;
+use std::env;
 use std::fs;
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
-use std::num::ParseIntError;
 use std::path::Path;
+use std::process::exit;
 use structopt::StructOpt;
-
-#[derive(Debug)]
-enum Error {
-    CliError,
-}
-
-impl From<ParseIntError> for Error {
-    fn from(_: ParseIntError) -> Error {
-        Error::CliError
-    }
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "error")
-    }
-}
-
-struct Input {
-    src: String,
-    dst: Option<String>,
-}
-
-impl Input {
-    fn default_dst(&self) -> String {
-        let mut s = self.src.clone();
-        s.push_str(".bin");
-        s
-    }
-}
-
-fn parse_input(s: &str) -> Result<Input, Error> {
-    lazy_static! {
-        static ref RE: Regex = Regex::new("^([^:]+)(?::(.+))?$").unwrap();
-    }
-    let caps = RE.captures(s).unwrap();
-    Ok(Input {
-        src: caps.get(1).unwrap().as_str().to_owned(),
-        dst: caps.get(2).map(|x| x.as_str().to_owned()),
-    })
-}
 
 #[derive(StructOpt)]
 struct Cli {
-    #[structopt(long = "input", short = "i", parse(try_from_str = parse_input))]
-    input: Vec<Input>,
+    #[structopt(name = "INPUT", help = "Input EWAL source files.")]
+    input: Vec<String>,
 
-    #[structopt(long = "build-tag", short = "t", default_value = "ephemeral")]
+    #[structopt(
+        long = "output",
+        short = "o",
+        help = "Output target directory. Will be created if not found. Stdout is - but will fail given multiple inputs."
+    )]
+    output_dir: Option<String>,
+
+    #[structopt(
+        long = "build-tag",
+        short = "t",
+        help = "Build tag compiled into the output binary.",
+        default_value = "ephemeral"
+    )]
     build_tag: String,
 }
 
-fn main() -> CliResult {
-    Ok(ewac_main()?)
+fn main() {
+    let args = Cli::from_args();
+    ewac_main(&args);
 }
 
-fn ewac_main() -> Result<(), failure::Error> {
-    let args = Cli::from_args();
-    let one_input = args.input.len() == 1;
-    let mut compiler = Compiler::new(args.build_tag.as_str());
-
-    for i in args.input {
-        let dst = match i.dst {
-            Some(o) => o,
-            None => {
-                if !one_input || atty::is(Stream::Stdout) {
-                    i.default_dst()
-                } else {
-                    "-".to_string()
-                }
-            }
-        };
-        let mut file = File::open(Path::new::<String>(&i.src))?;
-        let mut v = Vec::new();
-        let mut s = String::new();
-        if let Err(why) = file.read_to_string(&mut s) {
-            return Err(format_err!("failed to read input file: {}", why));
-        }
-        if let Err(why) = compiler.compile_to_writer(&mut v, s.as_str()) {
-            return Err(format_err!("failed to compile input file: {}", why));
-        }
-        if dst == "-" {
-            io::stdout().write_all(v.as_slice())?;
-            return Ok(());
-        }
-        if let Err(why) = fs::write(Path::new::<String>(&dst), v) {
-            return Err(format_err!("failed to write output file: {}", why));
-        }
+fn ewac_main(args: &Cli) {
+    let is_pipe = args.output_dir == Some("-".to_string()) || !atty::is(Stream::Stdout);
+    if is_pipe && args.input.len() != 1 {
+        eprintln!("Pipes are only supported with one input file.");
+        exit(1);
     }
 
-    Ok(())
+    if args.input.len() == 0 {
+        eprintln!("No input files.");
+        exit(1);
+    }
+
+    let curr_dir = env::current_dir().expect("Could not get current directory");
+    let output_dir = if let Some(dir) = args.output_dir.as_ref() {
+        let d = Path::new::<String>(&dir);
+        fs::create_dir_all(d).expect("Failed to create target directory");
+        d
+    } else {
+        let path = curr_dir
+            .to_str()
+            .expect("Current directory is not valid UTF-8");
+        Path::new::<str>(path)
+    };
+
+    let mut compiler = Compiler::new(args.build_tag.as_str());
+
+    for i in &args.input {
+        let filename = Path::new::<String>(&i);
+        let mut file = File::open(filename).expect("Failed to open input file");
+        let mut v = Vec::new();
+        let mut s = String::new();
+        file.read_to_string(&mut s)
+            .expect("Failed to read input file");
+        compiler
+            .compile_to_writer(&mut v, s.as_str())
+            .expect("Failed to compile input file");
+
+        if is_pipe {
+            io::stdout()
+                .write_all(v.as_slice())
+                .expect("Failed to write to stdout");
+        } else {
+            let path = output_dir.join(filename.file_stem().unwrap());
+            fs::write(path, v).expect("Failed to write target")
+        }
+    }
 }
